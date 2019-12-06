2e0a13ce6f5f06bd447cfab4f6f3bf4ba896cdf5
nguyenvannguyen <tieulongnuhd9x@gmail.com>
2019-12-06 16:30:55 +0900

    from2

--- moi.php
+++ moi.php
@@ -2,7 +2,10 @@
 session_start();
 ?>
 <?php
-if(isset($_SESSION['use']) && $_SESSION['pass']){
+if(isset($_SESSION['use']) && $_SESSION['pass'] != null){
   var_dump($_SESSION);
+?>
+<p><a href="logout.php">Logout</a><p>
+<?php
 }
 ?>
